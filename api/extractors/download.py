d193aa6f978cc99989db83fe371e4095d1b96f2f
Dheison Gomes <dheisomgomes0@gmail.com>
2023-08-08 10:44:49 -0300

    Fix links without full location

--- api/extractors/download.py
+++ api/extractors/download.py
@@ -30,10 +30,10 @@ async def get_download(path: str) -> Download:
     response = await http_get(f"{FRONT_PAGE}/{path}")
     soup = BeautifulSoup(response.text, "lxml")
 
-    clear = lambda s: s.replace("🔍", "").strip()
+    cleanText = lambda s: s.replace("🔍", "").strip()
 
-    title = clear(soup.find("div", class_="text-3xl font-bold").text)
-    authors = clear(soup.find("div", class_="italic").text)
+    title = cleanText(soup.find("div", class_="text-3xl font-bold").text)
+    authors = cleanText(soup.find("div", class_="italic").text)
     description = soup.find(name="div", class_="js-md5-top-box-description").text
     thumbnail = soup.find("img").get("src") or None
 
@@ -42,9 +42,10 @@ async def get_download(path: str) -> Download:
 
     raw_file_info = soup.find("div", class_="text-sm text-gray-500").text
     file_info = extract_file_info(raw_file_info)
-    download_links = list(
+    download_links = list(filter(
+        lambda i: i != None,
         map(parse_link, soup.find_all("a", class_="js-download-link"))
-    )
+    ))
 
     return Download(
         title,
@@ -58,5 +59,10 @@ async def get_download(path: str) -> Download:
     )
 
 
-def parse_link(link: Tag) -> Link:
-    return Link(title=link.text, url=link.get("href"))
+def parse_link(link: Tag) -> Link | None:
+    url = link.get("href")
+    if url == '/datasets':
+        return None
+    elif url[0] == '/':
+        url = FRONT_PAGE + url
+    return Link(link.text, url)
